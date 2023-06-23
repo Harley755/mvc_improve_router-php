e185f6ea8ee1def79d0bb018d373ebfd7795f8d3
Brice G <bricegoudalo@gmail.com>
2023-06-23 20:29:12 +0200

    fetch data from DB inside controller

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -2,12 +2,20 @@
 
 namespace Controllers;
 
+use Models\User;
 use Source\Renderer;
 
 class HomeController
 {
     public function index(): Renderer
     {
+        $userModel = new User();
+        $statement = $userModel->getPDO()->query('SELECT * FROM users');
+
+        foreach ($statement as $key => $user) {
+            var_dump($user);
+            echo '<br/>';
+        }
         return Renderer::make('home/index');
     }
 }
--- Models/User.php
+++ Models/User.php
@@ -1,7 +1,35 @@
 <?php
 
-namespace Models\User;
+namespace Models;
+
+use Source\Constant;
 
 class User
 {
+    private static \PDO $pdo;
+
+    public function __construct()
+    {
+        try {
+            static::$pdo = new \PDO(
+                'mysql:dbname=' . Constant::DB_NAME . ';host=' . Constant::DB_HOST,
+                Constant::DB_USERNAME,
+                Constant::DB_PASSWORD,
+                [
+                    // DONNEES RECUPEREES SOUS FORME D'OBJET
+                    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ,
+                    // AFFICHER LES ERREURS DE FACON CLAIRE
+                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+                ]
+            );
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+            die();
+        }
+    }
+
+    public function getPDO(): \PDO
+    {
+        return static::$pdo;
+    }
 }
